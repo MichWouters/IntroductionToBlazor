7595119540ffa170aecd238deab55efc89949b04
Michiel Wouters <michiel_woute64@hotmail.com>
2021-06-07 13:31:46 +0200

    Starting to work on Movies

---
+++ TodoBlazor/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoBlazor.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public double Rating { get; set; }
+    }
+}
